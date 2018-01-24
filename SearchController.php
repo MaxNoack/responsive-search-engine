c3fd7d8d038dec99cdef941ea5b9e3df58921530
MaxNoack <maxnoack@hotmail.com>
2018-01-24 11:38:02 +0100

    Change autoload method

--- SearchController.php
+++ SearchController.php
@@ -1,11 +1,10 @@
 <?php
 header('Content-Type: application/json');
 
-function __autoload($class_name) {
+spl_autoload_register(function ($class_name) {
 	$path = str_replace("_", "/", $class_name);
 	require_once $path . ".php";
-}
-
+});
 
 try {
 	$sub_string_array = [];
@@ -15,7 +14,6 @@ try {
 	$sub_string_array = $search_string->parse_search_string();
 	$data_source_connection->search_products($sub_string_array, $result_list);
 	$result_list->sort_descending();
-
 	echo json_encode(
 		array(
 			"result_list" => $result_list->get_results()
